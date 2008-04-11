726a2a878aceeeccb95a7b30dc304fa2f5102a75
Chris Anderson <jchris@grabb.it>
2008-04-11 03:40:58 -0700

    front page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+
+  def index
+    @title = "Welcome"
+  end
+  
+end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe PagesController do
+
+  #Delete this example and add some real ones
+  it "should use PagesController" do
+    controller.should be_an_instance_of(PagesController)
+  end
+
+end
