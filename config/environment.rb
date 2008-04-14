3193d0bfbe705fc54a868867f645c4337fd8b722
Chris Anderson <jchris@grabb.it>
2008-04-14 01:31:34 -0700

    optional exception notifier config

--- config/environment.rb
+++ config/environment.rb
@@ -57,3 +57,9 @@ Rails::Initializer.run do |config|
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
 end
+
+begin
+  require '/mnt/email.exceptions' 
+rescue LoadError 
+  nil
+end
